eb3bb7340ab01d7648a7951d6a922d84811c1e3a
jrah <jjames.home@gmail.com>
2018-03-18 14:11:16 +0000

    Add overlay js

---
+++ assets/js/overlayMenu.js
@@ -0,0 +1,11 @@
+const menuToggle = document.querySelector('.navMenu-toggle');
+
+menuToggle.addEventListener('click', (e) => {
+  const menu = document.querySelector('.navMenu');
+  const header = document.querySelector('header');
+  e.stopPropagation();
+  e.preventDefault();
+  header.classList.toggle('vh-100');
+  menu.classList.toggle('navMenu-isOpen');
+  document.body.classList.toggle('overflow-hidden');
+});
