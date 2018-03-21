f1398c6aa9cc28d86fdf3987bd4046465f101716
jrah <jjames.home@gmail.com>
2018-03-21 20:22:51 +0000

    Fix header to stick over main>header

--- assets/js/overlayMenu.js
+++ assets/js/overlayMenu.js
@@ -5,6 +5,7 @@ menuToggle.addEventListener('click', (e) => {
   const header = document.querySelector('header');
   e.stopPropagation();
   e.preventDefault();
+  menu.setAttribute('style', 'min-width: 100%;');
   header.classList.toggle('vh-100');
   menu.classList.toggle('navMenu-isOpen');
   document.body.classList.toggle('overflow-hidden');
