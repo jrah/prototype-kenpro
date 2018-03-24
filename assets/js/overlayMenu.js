8f501e31615ecd29aff6d8be8bb8fe092aa0c491
jrah <jjames.home@gmail.com>
2018-03-24 17:55:39 +0000

    Fix mobile overflow for header

--- assets/js/overlayMenu.js
+++ assets/js/overlayMenu.js
@@ -5,7 +5,6 @@ menuToggle.addEventListener('click', (e) => {
   const header = document.querySelector('header');
   e.stopPropagation();
   e.preventDefault();
-  menu.classList.toggle('w-100');
   header.classList.toggle('vh-100');
   menu.classList.toggle('navMenu-isOpen');
   document.body.classList.toggle('overflow-hidden');
